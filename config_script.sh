eacedf12e9d20f71cd303dbfbb4b5b955e63eba4
ASCTE-LJOHN <126697281+ASCTE-LJOHN@users.noreply.github.com>
2025-03-30 17:21:18 -0500

    attempt 1 wasd datapack

--- config_script.sh
+++
@@ -1,3 +0,0 @@
-# this script is used to configure all datapacks and mods to the appropriate settings
-# clear lag script runs more often (default 1000 sec)
-/scoreboard players set #CL_Time ClearLag 300
