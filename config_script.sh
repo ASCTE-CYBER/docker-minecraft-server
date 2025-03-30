89bcf32962f30e769e101b3dffdf22c403aa49f9
ASCTE-LJOHN <126697281+ASCTE-LJOHN@users.noreply.github.com>
2025-03-30 16:13:10 -0500

    updates

---
+++ config_script.sh
@@ -0,0 +1,3 @@
+# this script is used to configure all datapacks and mods to the appropriate settings
+# clear lag script runs more often (default 1000 sec)
+/scoreboard players set #CL_Time ClearLag 300
