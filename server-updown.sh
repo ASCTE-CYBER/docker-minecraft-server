f2f35b52ef1e5ccf76332cb2d3fc59068284b82d
ASCTE-LJOHN <126697281+ASCTE-LJOHN@users.noreply.github.com>
2025-04-05 09:34:57 -0500

    Update server-updown.sh

--- server-updown.sh
+++ server-updown.sh
@@ -6,30 +6,31 @@
 SCHOOL_START=800
 SCHOOL_END_MTh=1500
 SCHOOL_END_F=1300
-SERVER_STATE="-1"
-OVERRIDE="none"
+SERVER_STATE="undef"
+OVERRIDE="undef"
 
 while true; do
     DAY=$(date +%A)
     # calculate current time, remove leading zeroes, convert to numeric
     CURRENT_TIME=$(date +%H%M)
-    CURRENT_TIME=${CURRENT_TIME#0}
-    
+
     # Determine proper server state given time
     if [[ "$OVERRIDE" == "off" ]]; then
         SERVER_UP="0"
-    elif [[ "$CURRENT_TIME" -lt "$SCHOOL_START" ]]; then
+    elif [[ "$DAY" == "Saturday" || "$DAY" == "Sunday" ]]; then
         SERVER_UP="1"
-    elif [[ "$CURRENT_TIME" -gt "$SCHOOL_END_MTh" ]]; then
+    elif [[ "$((10#$CURRENT_TIME))" -lt "$SCHOOL_START" ]]; then
         SERVER_UP="1"
-    elif [[ "$DAY" == "Friday" && "$CURRENT_TIME" -gt "$SCHOOL_END_F" ]]; then
+    elif [[ "$((10#$CURRENT_TIME))" -gt "$SCHOOL_END_MTh" ]]; then
+        SERVER_UP="1"
+    elif [[ "$DAY" == "Friday" && "$((10#$CURRENT_TIME))" -gt "$SCHOOL_END_F" ]]; then
         SERVER_UP="1"
     elif [[ "$OVERRIDE" == "on" ]]; then
         SERVER_UP="1"
     else
         SERVER_UP="0"
     fi
-    
+
     # Start/stop the server accordingly
     if [[ "$SERVER_UP" == "1" && "$SERVER_STATE" != "1" ]]; then
         docker start chaos && SERVER_STATE="1"
@@ -38,9 +39,7 @@ while true; do
         docker stop chaos && SERVER_STATE="0"
         echo "Stopped server at $(date)."
     fi
-    
+
     # check again in 1 minute
     sleep 59
 done
-
-
